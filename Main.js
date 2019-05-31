b7457e63dcbc5f9788ef91d63e6c742d98fb66b4
Heyjoy <iheyjoy@gmail.com>
2019-06-01 03:33:40 +0800

    right way approch

--- Main.js
+++ Main.js
@@ -2,6 +2,8 @@
 const video1 = document.getElementById('video1');
 const video2 = document.getElementById('video2');
 const video3 = document.getElementById('video3');
+const VedioSelectBox = document.getElementById("vedioSelect");
+const output = document.getElementById("listing");
 var files;
 
 function playPause() {
@@ -11,18 +13,24 @@ function playPause() {
     video1.pause();
 }
 document.getElementById("filepicker").addEventListener("change", function(event) {
-  let output = document.getElementById("listing");
   files = event.target.files;
-
+  VedioSelectBox.innerHTML = ""; // clean up the list
   const obj_url = window.URL.createObjectURL(files[2]);
   for (let i=0; i<files.length; i++) {
-    let item = document.createElement("OPTION");
-    item.innerHTML = files[i].name;
-    document.getElementById("mySelect").appendChild(item);
+    let item_opt = document.createElement("OPTION");
+    item_opt.innerHTML = files[i].name;
+    VedioSelectBox.appendChild(item_opt);
   };
   video1.src = obj_url;
   video1.load()
 }, false);
+
+function listVedio(){
+  let item_li = document.createElement("li");
+  item_li.innerHTML = VedioSelectBox.value;
+  document.getElementById("demo").innerHTML = "You selected: ";
+  output.appendChild(item_li);
+}
 
     // listVedio(filename);
 
