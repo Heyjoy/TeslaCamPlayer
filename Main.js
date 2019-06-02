4cc29ad726d6e82077c6e95d7154b5049cf3f4d1
Heyjoy <iheyjoy@gmail.com>
2019-06-02 23:13:03 +0800

    works now

--- Main.js
+++ Main.js
@@ -1,24 +1,16 @@
 
-const video1 = document.getElementById('video1');
-const video2 = document.getElementById('video2');
-const video3 = document.getElementById('video3');
+const videoes = new Array(document.getElementById('video1'),document.getElementById('video2'),document.getElementById('video3'))
 const VedioSelectBox = document.getElementById("vedioSelect");
 const output = document.getElementById("listing");
 var files;
+var playPauseFlag = false;
 
-function playPause() {
-  if (video1.paused)
-    video1.play();
-  else
-    video1.pause();
-}
 document.getElementById("filepicker").addEventListener("change", function(event) {
   files = event.target.files;
   VedioSelectBox.innerHTML = ""; // clean up the list
-
   for (let i=0; i<files.length; i++) {
     let item_opt = document.createElement("OPTION");
-    item_opt.innerHTML = window.URL.createObjectURL(files[i]);
+    item_opt.innerHTML = files[i].name;
     VedioSelectBox.appendChild(item_opt);
   };
 
@@ -27,17 +19,47 @@ document.getElementById("filepicker").addEventListener("change", function(event)
 function listVedio(){
   let item_Index = VedioSelectBox.selectedIndex;
   let item_li = document.createElement("li");
-  item_li.innerHTML = VedioSelectBox.selectedIndex;
+
+  // item_li.innerHTML = checkName;
+  for(x in files){
+    if (files[x].name == VedioSelectBox.value){
+      item_li.innerHTML = "x";
+    }
+    else {
+      item_li.innerHTML = "found nothing";
+    }
+  }
+  let vedio_Index = Math.floor((item_Index+1)/3);
+  let vedio_IdBase = vedio_Index*3;
+  item_li.innerHTML = "[No."+VedioSelectBox.selectedIndex+"]"+VedioSelectBox.value+"  VedioIdex:"+vedio_IdBase;
   output.appendChild(item_li);
-  const obj_url = window.URL.createObjectURL(files[item_Index]);
-  video1.src = obj_url;
-  video1.load()
+
+  for(var i=0;i<3;i++){
+    let file = files[vedio_Index*3+i];
+    let obj_url =  window.URL.createObjectURL(file);
+    alert(file.name);
+    videoes[i].src =obj_url;
+    videoes[i].load()
+  }
+
 }
+//display date
+function displayVedioContent{
 
-    // listVedio(filename);
 
-    // var date= filename.split("-");
-    // // document.write(date)
-    // myVideos.src='C:\Users\heyjoy\Desktop\2019-05-10_20-38-29\'+files[0];
-    // myVideos.load();
-  // }
+}
+// add vedio control
+function playPause() {
+  if (playPauseFlag!=true){
+    for (var i=0;i<3;i++){
+      videoes[i].play();
+    }
+    playPauseFlag = true;
+  }
+  else{
+    for (var i=0;i<3;i++){
+      videoes[i].pause();
+    }
+    playPauseFlag = false;
+  }
+}
